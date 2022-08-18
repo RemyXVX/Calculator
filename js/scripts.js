06af9715ac4d188ae0b61e77d132077339adfb1b
RemyXVX <remyfranciscoflores@gmail.com>
2022-08-17 20:05:12 -0700

    readme

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2
+}
+
+function divide(number1, number2) {
+  return number1 / number2
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+window.alert ("The sum of the equation is " + (add(number1, number2)));
+window.alert("The difference of the equation is " + (subtract(number1, number2)));
+window.alert("The product of the equation is " + (multiply(number1, number2)));
+window.alert("The dividend of the equation is " + (divide(number1, number2)));
+
+
